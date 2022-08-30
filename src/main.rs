c2ca2266dfbf711df4ab6fd476d0a7ea28988e93
josiahdowdy <josiahdowdy@protonmail.com>
2022-08-30 09:44:07 -0600

    Added KayakUI

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 use bevy::prelude::*;
 
+use bevy::{
+    input::mouse::{MouseScrollUnit, MouseWheel}
+};
+use kayak_ui::bevy::BevyKayakUIPlugin;
+
+mod button;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
+        .add_system(button::button_system)
+        .add_plugin(BevyKayakUIPlugin)
         .run();
 }
 
@@ -13,4 +22,10 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         texture: asset_server.load("icon.png"),
         ..Default::default()
     });
+
+    // Camera
+    commands.spawn_bundle(Camera2dBundle::default());
+
+   
 }
+
